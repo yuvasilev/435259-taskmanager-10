5432947301997f73d3b7193aecaacb8ea0cc826b
Anna <annaa_bananaa@mail.ru>
2019-11-21 13:32:36 +0300

    Первое задание

--- src/main.js
+++ src/main.js
@@ -386,7 +386,79 @@ const createTascEditTEmplate =() => {
   );
 };
 
-const createMoreButtonTamplate = () =>{
+const creatTasckTemplate =() => {
+  return (
+  `
+  <article class="card card--black">
+    <div class="card__form">
+      <div class="card__inner">
+        <div class="card__control">
+          <button type="button" class="card__btn card__btn--edit">
+            edit
+          </button>
+          <button type="button" class="card__btn card__btn--archive">
+            archive
+          </button>
+          <button
+            type="button"
+            class="card__btn card__btn--favorites card__btn--disabled"
+          >
+            favorites
+          </button>
+        </div>
+
+        <div class="card__color-bar">
+          <svg class="card__color-bar-wave" width="100%" height="10">
+            <use xlink:href="#wave"></use>
+          </svg>
+        </div>
+
+        <div class="card__textarea-wrap">
+          <p class="card__text">Example default task with default color.</p>
+        </div>
+
+        <div class="card__settings">
+          <div class="card__details">
+            <div class="card__dates">
+              <div class="card__date-deadline">
+                <p class="card__input-deadline-wrap">
+                  <span class="card__date">23 September</span>
+                  <span class="card__time">11:15 PM</span>
+                </p>
+              </div>
+            </div>
+
+            <div class="card__hashtag">
+              <div class="card__hashtag-list">
+                <span class="card__hashtag-inner">
+                  <span class="card__hashtag-name">
+                    #todo
+                  </span>
+                </span>
+
+                <span class="card__hashtag-inner">
+                  <span class="card__hashtag-name">
+                    #personal
+                  </span>
+                </span>
+
+                <span class="card__hashtag-inner">
+                  <span class="card__hashtag-name">
+                    #important
+                  </span>
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </article>
+  `
+  );
+};
+
+const createMoreButtonTamplate = () => {
   return(
   `
   <button class="load-more" type="button">load more</button>
@@ -410,6 +482,14 @@ render(siteMainElement, createBoardTemplate(), `beforeEnd`);
 
 const taskTasksElement = document.querySelector(`.board__tasks`);
 render(taskTasksElement, createTascEditTEmplate(), `beforeEnd`);
+
+
+const tasksArray = ['', '', ''];
+
+const mapTasksCount = tasksArray.map((tasksArray) => {
+  render(taskTasksElement, creatTasckTemplate(),`beforeEnd`);
+});
+
 
 const boardElement = document.querySelector(`.board`);
 render(boardElement, createMoreButtonTamplate(), `beforeEnd`);
